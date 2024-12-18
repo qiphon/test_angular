1d130bddcbcaad4644f1cb8a573c689a9c777cb1
qiphon <num1103@163.com>
2024-12-18 13:20:33 +0800

    补充 组件 template 模式

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router'
 import { FirstComponent } from '../firstComponent/firstComponent.comp'
+import { FirstTemplate } from '../firstTemplate/firstTemplate.comp'
 
 export const routes: Routes = [
   {
     title: 'first component',
     path: 'first',
     component: FirstComponent,
+  },
+  {
+    title: 'FirstTemplate',
+    path: 'first_temp',
+    component: FirstTemplate,
   },
 ]
---
+++ src/firstTemplate/firstTemplate.comp.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  templateUrl: './firstTemplate.comp.html',
+  styleUrl: './firstTemplate.comp.less',
+  selector: 'first-temp',
+})
+export class FirstTemplate {}
