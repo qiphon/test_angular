ab24bb8885f761a111051a51a60331d5ea70990d
qiphon <num1103@163.com>
2024-12-20 11:01:47 +0800

    add signal equal 判断函数

--- src/firstSignal/firstSignal.comp.ts
+++ src/firstSignal/firstSignal.comp.ts
@@ -1,4 +1,4 @@
-import { Component, computed, effect, signal } from '@angular/core'
+import { Component, computed, effect, signal, untracked } from '@angular/core'
 
 @Component({
   templateUrl: './firstSignal.comp.html',
@@ -10,6 +10,8 @@ export class FirstSignal {
   countString = signal('0')
   computedValue = computed(() => this.count() * 4)
   arr = [1, 2, 3, 4]
+  sameValTest = signal(['1'], { equal: (a, b) => Object.is(a[0], b[0]) })
+  sameValNotEqualFn = signal(['1'])
 
   constructor() {
     const destroyCountEffect = effect(() => {
@@ -32,11 +34,35 @@ export class FirstSignal {
     })
   }
 
+  setEqualValue() {
+    console.log('set equal value')
+    this.sameValNotEqualFn.set(['1'])
+    this.sameValTest.set(['1'])
+  }
+
+  private equalEffect = effect(() => {
+    console.log(
+      '----------equalEffect , set same value will not rerender ---------',
+    )
+    console.log(this.sameValTest())
+  })
+  private notEqualEffect = effect(() => {
+    console.log('----------not equalEffect---------')
+    console.log(this.sameValNotEqualFn())
+  })
+  private notEqualEffectAddUntrack = effect(() => {
+    console.log(
+      '----------not notEqualEffectAddUntrack--- count change will not log ------',
+    )
+    console.log(this.sameValNotEqualFn(), untracked(this.count))
+  })
+
   private logCountEffect = effect(() => {
     console.log('private log effect , not in constructor')
     console.log(this.computedValue())
   })
-  // need pass injector in constuctor
+
+  // TODO need pass injector in constuctor
   // private logCountStringEffect() {
   //   return effect(() => {
   //     console.log('log string effect')
