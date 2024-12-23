e95bd403e0df87a5290af3a9ddc34b4b1332d848
qiphon <num1103@163.com>
2024-12-23 19:42:19 +0800

    feat: add css deep

--- src/firstComponent/firstComponent.comp.ts
+++ src/firstComponent/firstComponent.comp.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core'
 
 @Component({
   template: `
-    <h1>FirstComponent h1</h1>
-    <p>this is the first time on it</p>
+    <div>
+      <h1>FirstComponent h1</h1>
+      <p>this is the first time on it</p>
+    </div>
   `,
   styles: `
     h1 {
       color: red;
     }
   `,
-  selector: 'first-comp',
+  selector: 'first-comp, aaa',
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class FirstComponent {}
--- src/useSubComp/components/desc/desc.comp.ts
+++ src/useSubComp/components/desc/desc.comp.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core'
 
 @Component({
-  template: `<h4>desc comp</h4>`,
-  selector: 'desc',
+  template: `<h4>desc comp <span>desc span</span></h4>`,
+  selector: 'descA',
 })
 export class Desc {}
